77b9ceb352c6c6869b98ea5f3299697de8d656ea
Andres <andrescondegarcia11@gmail.com>
2025-03-06 09:41:00 +0100

    Projecto vacio para examen

---
+++ src/test/java/com/example/examenjpa/ExamenJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
